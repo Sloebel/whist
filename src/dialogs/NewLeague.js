ed765b4608aecc613520d609d76069a2e209b440
Sloebel <shahar.lebel@gmail.com>
2018-08-19 17:44:17 +0300

    new players loader and some small fixes

--- src/dialogs/NewLeague.js
+++ src/dialogs/NewLeague.js
@@ -7,8 +7,6 @@ import AddPlayerSmall from './../players/AddPlayerSmall';
 const FormItem = Form.Item;
 const Option = Select.Option;
 const Panel = Collapse.Panel;
-console.log(Collapse)
-
 
 const NewLeague = Form.create()(
     class extends Component {
@@ -17,7 +15,9 @@ const NewLeague = Form.create()(
             count: 0,
             loading: true,
             selectedRowKeys: [], // Check here to configure the default column
-            addPlayerCollapse: []
+            addPlayerCollapse: [],
+            newPlayerLoader: false,
+            savingLeagueLoader: false
         };
 
         onSelectChange = (selectedRowKeys) => {
@@ -51,24 +51,28 @@ const NewLeague = Form.create()(
 
         addPlayerCallback(values) {
             return new Promise((resolve, reject) => {
-                    this.setState({ loading: true });
-                    fire.database().ref('players').once('value', snapshot => {
+                this.setState({ newPlayerLoader: true });
+                fire.database().ref('players').once('value', snapshot => {
 
-                        const newID = snapshot.val().lastID + 1;
+                    const newID = snapshot.val().lastID + 1;
+                    const playersValue = this.playersSelect.props.value || [];
 
-                        fire.database().ref('players/list/_' + newID).set({ ...values, playerID: newID });
-                        fire.database().ref('players/lastID').set(newID);
-                        this.setState({ loading: false, addPlayerCollapse: [] });
-                        resolve(true);
+                    fire.database().ref('players/list/_' + newID).set({ ...values, playerID: newID }, () => {
+                        this.props.form.setFieldsValue({ players: playersValue.concat([values.nickname]) });
+                        this.focusPlayersSelect();
                     });
-                })
+                    fire.database().ref('players/lastID').set(newID);
+                    this.setState({ newPlayerLoader: false, addPlayerCollapse: [] });
+                    resolve(true);
+                });
+            })
         }
 
 
         inlinePlayerFields(arr) {
             const { form } = this.props;
             const { getFieldDecorator } = form;
-            console.log('inlinePlayerFields');
+
             return arr.map((v, i) => <FormItem label={'Player ' + (v + i)} key={'player_' + (v + i)}>
                 <Col span={12} style={{ paddingRight: '4px' }}>
                     <FormItem>
@@ -91,9 +95,13 @@ const NewLeague = Form.create()(
             </FormItem>);
         }
 
+        focusPlayersSelect = function () {
+            this.playersSelect.focus();
+        }
+
         render() {
             const { visible, onCancel, content, form } = this.props;
-            const { players, loading } = this.state;
+            const { players, loading, newPlayerLoader, savingLeagueLoader } = this.state;
             const { getFieldDecorator } = form;
             const onOk = () => {
                 const fieldsValues = form.getFieldsValue();
@@ -115,13 +123,11 @@ const NewLeague = Form.create()(
                     return true;
                 });
 
-                console.log('myOk');
-                console.log(fieldsValues);
-
                 form.validateFields(fieldsNames, (err, values) => {
                     if (!err) {
                         console.log('Received values of form: ', values);
                         const { names, nicknames } = values;
+                        this.setState({ savingLeagueLoader: true });
 
                         if (names) {
                             let playerID;
@@ -142,6 +148,7 @@ const NewLeague = Form.create()(
                             playersRef.child('lastID').set(playerID);
                         }
 
+                        this.setState({ savingLeagueLoader: false });
                         onCancel();
                     }
                 });
@@ -152,6 +159,8 @@ const NewLeague = Form.create()(
                     title="Create League"
                     visible={visible}
                     destroyOnClose={true}
+                    maskClosable={false}
+                    confirmLoading={savingLeagueLoader}
                     onOk={onOk}
                     onCancel={onCancel}
                     width="600px"
@@ -168,16 +177,17 @@ const NewLeague = Form.create()(
                         <FormItem label="Description">
                             {getFieldDecorator('description')(<Input type="textarea" />)}
                         </FormItem>
-                        {players.length ?
+                        {loading ? <Spin size="small" style={{ width: '100%', marginBottom: '24px', height: '69px', paddingBottom: '8px' }} /> : players.length ?
                             <FormItem label="Players">
                                 {getFieldDecorator('players', {
                                     rules: [{ required: true, len: 4, type: 'array' }],
                                 })(
                                     <Select
                                         mode="multiple"
+                                        ref={node => this.playersSelect = node}
                                     // notFoundContent={loading ? <Spin size="small" /> : 'add players'}
                                     >
-                                        {players.map(p => <Option key={p.playerID} value={p.playerID}>{p.nickname}</Option>)}
+                                        {players.map(p => <Option key={p.playerID} value={p.nickname}>{p.nickname}</Option>)}
                                     </Select>
                                 )}
                             </FormItem> :
@@ -187,7 +197,7 @@ const NewLeague = Form.create()(
                     {players.length ?
                         <Collapse bordered={false} activeKey={this.state.addPlayerCollapse} onChange={this.addPlayerCollapseChange}>
                             <Panel header="add player" key="1">
-                                <AddPlayerSmall callback={this.addPlayerCallback.bind(this)} players={this.state.players} loading={loading}/>
+                                <AddPlayerSmall callback={this.addPlayerCallback.bind(this)} players={this.state.players} loading={newPlayerLoader} />
                             </Panel>
                         </Collapse> : ''}
 
@@ -197,74 +207,4 @@ const NewLeague = Form.create()(
         }
     }
 );
-
-
-// class NewLeague extends Component {
-//     state = {
-//         // dataSource: [{
-//         //     key: '0',
-//         //     name: 'Edward King 0',
-//         //     nickName: 'nickName 0'
-//         // }, {
-//         //     key: '1',
-//         //     name: 'Edward King 1',
-//         //     nickName: 'nickName 1',
-//         // }],
-//         // count: 2,
-//         dataSource: [],
-//         count: 0,
-//         loading: true,
-//         selectedRowKeys: [], // Check here to configure the default column
-//     };
-
-//     onSelectChange = (selectedRowKeys) => {
-//         this.setState({ selectedRowKeys });
-//     }
-
-
-//     fetch() {
-//         /* Create reference to messages in Firebase Database */
-//         let messagesRef = fire.database().ref('players').orderByKey().limitToLast(100);
-//         messagesRef.on('value', snapshot => {
-//             /* Update React state when message is added at Firebase Database */
-//             // let message = { text: snapshot.val(), id: snapshot.key };
-//             if (snapshot.val()) {
-//                 this.setState({ dataSource: [snapshot.val()].concat(this.state.dataSource), loading: false });
-//             } else {
-//                 this.setState({ loading: false });
-//             }
-
-//             console.log(snapshot.val());
-//         })
-//     }
-
-
-
-//     // componentDidMount() {
-//     //     this.fetch();
-//     // }
-
-//     render() {
-//         const { visible, onCancel, content } = this.props;
-//         const onOk = () => {
-//             console.log('myOk');
-//             onCancel()
-//         };
-
-//         return (
-//             <Modal
-//                 title="Create League"
-//                 visible={visible}
-//                 destroyOnClose={true}
-//                 onOk={onOk}
-//                 onCancel={onCancel}
-//                 width="600px"
-//                 okText="Save & Play"
-//             >
-//                 <SelectionTool {...this.state} onSelectChange={this.onSelectChange} fetch={this.fetch.bind(this)} />
-//             </Modal>
-//         )
-//     }
-// }
-
 export default NewLeague;
