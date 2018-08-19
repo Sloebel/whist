013d0004757bd6109b68766516a7cde2afc604f9
Sloebel <shahar.lebel@gmail.com>
2018-08-19 15:30:25 +0300

    changing to personal account

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyCuFeD-S6aBiRCewbczDjxIBahOKONSepU",
-    authDomain: "whist-83fed.firebaseapp.com",
-    databaseURL: "https://whist-83fed.firebaseio.com",
-    projectId: "whist-83fed",
-    storageBucket: "whist-83fed.appspot.com",
-    messagingSenderId: "646698326394"
+    apiKey: "AIzaSyCBrRBBfQTCAF5rqj_A8fXN4jMZlZ310RM",
+    authDomain: "whist-d4d9b.firebaseapp.com",
+    databaseURL: "https://whist-d4d9b.firebaseio.com",
+    projectId: "whist-d4d9b",
+    storageBucket: "",
+    messagingSenderId: "630677405872"
 };
 const fire = firebase.initializeApp(config);
 
