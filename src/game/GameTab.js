3f88e8260a2e55795c7bd493618c8a0d241f234c
Shahar Loebel <shahar.lebel@gmail.com>
2018-10-02 22:03:32 +0300

    editing game table work in progress

--- src/game/GameTab.js
+++ src/game/GameTab.js
@@ -1,86 +1,113 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import fire from './../fire.js';
 import { Tabs, Table } from 'antd';
+import { EditableFormRow, EditableCell } from './../common/table/EditableCell.js'
 import './GameTab.css';
 
 const TabPane = Tabs.TabPane;
 
 class GameTab extends TabPane {
+	constructor(props) {
+	    super(props);
+	
+	    this.columns = [{
+	        children: [{
+	            title: 'Score',
+	            children: [{
+	                title: 'round',
+	                dataIndex: 'round',
+	                width: 75,
+	            }]
+	        }]
+	    }, {
+	        title: 'player 1',
+	        editable: true,
+	        children: [{
+	            title: 'score',
+	            children: [{
+	                title: 'Bid',
+	                dataIndex: 'bid1',
+	                width: 100,
+	                onCell: record => ({
+			          record,
+			          editable: true,
+			          dataIndex: 'bid1',
+			          title: 'Bid',
+			          handleSave: this.handleSave,
+			        }),
+	            }, {
+	                title: 'Won',
+	                dataIndex: 'won1',
+	                width: 100,
+	            }]
+	        }],
+	    }, {
+	        title: 'player 2',
+	        children: [{
+	            title: 'score',
+	            children: [{
+	                title: 'Bid',
+	                dataIndex: 'bid2',
+	                width: 100,
+	            }, {
+	                title: 'Won',
+	                dataIndex: 'won2',
+	                width: 100,
+	            }]
+	        }],
+	    }, {
+	        title: 'player 3',
+	        children: [{
+	            title: 'score',
+	            children: [{
+	                title: 'Bid',
+	                dataIndex: 'bid3',
+	                width: 100,
+	            }, {
+	                title: 'Won',
+	                dataIndex: 'won3',
+	                width: 100,
+	            }]
+	        }],
+	    }, {
+	        title: 'player 4',
+	        children: [{
+	            title: 'score',
+	            children: [{
+	                title: 'Bid',
+	                dataIndex: 'bid4',
+	                width: 100,
+	            }, {
+	                title: 'Won',
+	                dataIndex: 'won4',
+	                width: 100,
+	            }]
+	        }],
+	    }];
+	}
 
-    render() {
-        const columns = [{
-            children: [{
-                title: 'Score',
-                children: [{
-                    title: 'round',
-                    dataIndex: 'round',
-                    width: 75,
-                }]
-            }]
-        }, {
-            title: 'player 1',
-            children: [{
-                title: 'score',
-                children: [{
-                    title: 'Bid',
-                    dataIndex: 'bid1',
-                    width: 100,
-                }, {
-                    title: 'Won',
-                    dataIndex: 'won1',
-                    width: 100,
-                }]
-            }],
-        }, {
-            title: 'player 2',
-            children: [{
-                title: 'score',
-                children: [{
-                    title: 'Bid',
-                    dataIndex: 'bid2',
-                    width: 100,
-                }, {
-                    title: 'Won',
-                    dataIndex: 'won2',
-                    width: 100,
-                }]
-            }],
-        }, {
-            title: 'player 3',
-            children: [{
-                title: 'score',
-                children: [{
-                    title: 'Bid',
-                    dataIndex: 'bid3',
-                    width: 100,
-                }, {
-                    title: 'Won',
-                    dataIndex: 'won3',
-                    width: 100,
-                }]
-            }],
-        }, {
-            title: 'player 4',
-            children: [{
-                title: 'score',
-                children: [{
-                    title: 'Bid',
-                    dataIndex: 'bid4',
-                    width: 100,
-                }, {
-                    title: 'Won',
-                    dataIndex: 'won4',
-                    width: 100,
-                }]
-            }],
-        }];
+	handleSave = (row) => {}
+    render() {       
+
+     //    const columns = this.columns.map((col) => {      
+	    //   return {
+	    //     ...col,
+	    //     onCell: record => ({
+	    //       record,
+	    //       editable: col.editable,
+	    //       dataIndex: col.dataIndex,
+	    //       title: col.title,
+	    //       handleSave: this.handleSave,
+	    //     }),
+	    //   };
+	    // });
 
         const dataSource = []
 
         for (let i = 0; i < 13; i++) {
             dataSource.push({
                 round: i + 1,
-                bid1: null,
+                bid1: '',
                 won1: null,
                 bid2: null,
                 won2: null,
@@ -91,16 +118,24 @@ class GameTab extends TabPane {
             });
         }
 
+        const components = {
+	      body: {
+	        row: EditableFormRow,
+	        cell: EditableCell,
+	      },
+	    };
+
         return (
             <Table
                 className='game-table'
-                columns={columns}
+                components={components}
+                columns={this.columns}
                 rowKey='round'
                 dataSource={dataSource}
                 size='small'
                 bordered
                 pagination={false}
-            // scroll={{ y: 520 }}
+            	scroll={{ y: 520 }}
             />
         );
     }
