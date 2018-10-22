2da2b6e4443819741fe0d8b081c300b17e4ba7ff
Sloebel <shahar.lebel@gmail.com>
2018-10-22 17:21:46 +0300

    changing to functional component

--- src/dialogs/Dialog.js
+++ src/dialogs/Dialog.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import NewLeagueDialog from './NewLeague';
 import ResumeLeagueDialog from './ResumeLeague';
 
-class Dialog extends Component {
-    dialogs = {
+const Dialog = ({
+    dialog,
+    dialogProps
+}) => {
+    const dialogs = {
         newLeague: NewLeagueDialog,
         resumeLeague: ResumeLeagueDialog
     };
-    render() {
-        const { dialog, dialogProps } = this.props;
-        const DialogEl = this.dialogs[dialog];
-        return <DialogEl {...dialogProps}/>
-    }
+    const DialogEl = dialogs[dialog];
+
+    return <DialogEl {...dialogProps} />;
 }
+
 export default Dialog;
 
