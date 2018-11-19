6b7141e0666ed1edb353feecdaded3f505971e25
Sloebel <shahar.lebel@gmail.com>
2018-11-19 14:21:26 +0200

    theming

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    config = injectBabelPlugin(
+        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // change importing css to less
+        config,
+    );
+
+    config = rewireLess.withLoaderOptions({
+        modifyVars: {
+            "@primary-color": "#118060",
+            "@border-radius-base": "0px"
+        },
+        javascriptEnabled: true,
+    })(config, env);
+
+    return config;
+};
