6423148a6d1d00240dc97e05e1e60edd542631a7
Shahar Loebel <shaharl@cb4.com>
2019-04-28 23:41:05 +0300

    filtering list of leagues by userId

--- src/main/Main.js
+++ src/main/Main.js
@@ -5,102 +5,112 @@ import MainMenu from './MainMenu';
 import * as routes from './../constants/routes';
 
 class Main extends Component {
-    // Dialogs visible states controling if the Dialog should be instansiated or not
-    // this outer state is for saving unnecessary access to the DB
-    state = {
-        activeLeagues: [],
-        newLeague: false,
-        resumeLeague: false
-    };
+  // Dialogs visible states controling if the Dialog should be instansiated or not
+  // this outer state is for saving unnecessary access to the DB
+  state = {
+    activeLeagues: [],
+    newLeague: false,
+    resumeLeague: false
+  };
 
-    /* Create reference to activeLeagues in Firebase Database */
-    activeLeagues = fire.database().ref('leagues/list').orderByChild("active").equalTo(true);
+  /* Create reference to activeLeagues in Firebase Database */
+  activeLeagues = fire.database().ref('leagues/list').orderByChild("active").equalTo(true);
 
-    fetch() {
-        this.activeLeagues.on('value', snapshot => {
-            if (snapshot.val()) {
-                this.setState({ activeLeagues: Object.values(snapshot.val()), loading: false });
+  fetch() {
+    this.activeLeagues.on('value', snapshot => {
+      if (snapshot.val()) {
+        const userId = fire.auth().currentUser.uid;
+
+        // filtering leagues data by userId,
+        // TODO: check if possible to fix the data differently in the database for better queries
+        // TODO: fix the loader functionality    
+        this.setState({
+          activeLeagues: Object.values(snapshot.val())
+            .filter(({ players }) => Object.values(players)
+              .some(({ key }) => key === userId)),
+          loading: false
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  componentWillUnmount() {
+    this.activeLeagues.off('value');
+  }
+
+
+  showResumeLeagueDialog() {
+    this.setState({
+      resumeLeague: true
+    });
+  }
+
+  closeResumeLeagueDialog(leagueID) {
+    this.setState({
+      resumeLeague: false
+    });
+
+    this.navigateToLeague(leagueID);
+  }
+
+  showNewLeagueDialog() {
+    this.setState({
+      newLeague: true
+    });
+  }
+
+  closeNewLeagueDialog(leagueID) {
+    this.setState({
+      newLeague: false
+    });
+
+    this.navigateToLeague(leagueID);
+  }
+
+  navigateToLeague(leagueID) {
+    if (leagueID) {
+      this.props.history.push(`${routes.LEAGUE}/` + leagueID)
+    }
+  }
+
+  render() {
+    return (
+      <div className="app-main">
+        <MainMenu items={[
+          {
+            text: 'Resume League',
+            onClick: this.showResumeLeagueDialog.bind(this),
+            dialog: 'resumeLeague',
+            disabled: this.state.activeLeagues.length === 0,
+            dialogProps: {
+              afterClose: this.closeResumeLeagueDialog.bind(this),
+              visible: this.state.resumeLeague,
+              activeLeagues: this.state.activeLeagues
             }
-        });
-    }
-
-    componentDidMount() {
-        this.fetch();
-    }
-
-    componentWillUnmount() {
-        this.activeLeagues.off('value');
-    }
-
-
-    showResumeLeagueDialog() {
-        this.setState({
-            resumeLeague: true
-        });
-    }
-
-    closeResumeLeagueDialog(leagueID) {
-        this.setState({
-            resumeLeague: false
-        });
-
-        this.navigateToLeague(leagueID);
-    }
-
-    showNewLeagueDialog() {
-        this.setState({
-            newLeague: true
-        });
-    }
-
-    closeNewLeagueDialog(leagueID) {
-        this.setState({
-            newLeague: false
-        });
-
-        this.navigateToLeague(leagueID);
-    }
-
-    navigateToLeague(leagueID) {
-        if (leagueID) {
-            this.props.history.push(`${routes.LEAGUE}/` + leagueID)
-        }
-    }
-
-    render() {
-        return (
-            <div className="app-main">
-                <MainMenu items={[
-                    {
-                        text: 'Resume League',
-                        onClick: this.showResumeLeagueDialog.bind(this),
-                        dialog: 'resumeLeague',
-                        disabled: this.state.activeLeagues.length === 0,
-                        dialogProps: {
-                            afterClose: this.closeResumeLeagueDialog.bind(this),
-                            visible: this.state.resumeLeague,
-                            activeLeagues: this.state.activeLeagues
-                        }
-                    }, {
-                        text: 'Create League',
-                        onClick: this.showNewLeagueDialog.bind(this),
-                        dialog: 'newLeague',
-                        dialogProps: {
-                            afterClose: this.closeNewLeagueDialog.bind(this),
-                            visible: this.state.newLeague
-                        }
-                    },
-                    { text: 'Settings' },
-                    { text: 'Scores' },
-                    {
-                        text: 'Sign Out',
-                        onClick: auth.signOut
-                    }
-                ]}
-                />
-            </div>
-        );
-    }
+          }, {
+            text: 'Create League',
+            onClick: this.showNewLeagueDialog.bind(this),
+            dialog: 'newLeague',
+            dialogProps: {
+              afterClose: this.closeNewLeagueDialog.bind(this),
+              visible: this.state.newLeague
+            }
+          },
+          { text: 'Settings' },
+          { text: 'Scores' },
+          {
+            text: 'Sign Out',
+            onClick: auth.signOut
+          }
+        ]}
+        />
+      </div>
+    );
+  }
 }
 
 const authCondition = (authUser) => !!authUser;
