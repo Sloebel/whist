d0f0b79b1c04c9ee2a4842f4fc5996daf4413b79
Shahar Loebel <shaharl@cb4.com>
2020-03-29 00:23:58 +0300

    Updates

---
+++ precache-manifest.d3703f69205a08cb06c4ceab0887ee9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789cd3f07a50a55441869a121765d7ea",
+    "url": "/whist/index.html"
+  },
+  {
+    "revision": "054832bfb8a379e65430",
+    "url": "/whist/static/css/2.c02d0dce.chunk.css"
+  },
+  {
+    "revision": "359ccec11587916fbe0c",
+    "url": "/whist/static/css/main.9f56b6c0.chunk.css"
+  },
+  {
+    "revision": "054832bfb8a379e65430",
+    "url": "/whist/static/js/2.1d09c07b.chunk.js"
+  },
+  {
+    "revision": "c6d260ff1985c9ac7e3e5450022a63d3",
+    "url": "/whist/static/js/2.1d09c07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359ccec11587916fbe0c",
+    "url": "/whist/static/js/main.8245c162.chunk.js"
+  },
+  {
+    "revision": "a071035df8ec3812ae75",
+    "url": "/whist/static/js/runtime-main.aef0a735.js"
+  },
+  {
+    "revision": "03003d3d190f3443f49e966f7ff362bf",
+    "url": "/whist/static/media/cards.03003d3d.png"
+  }
+]);
