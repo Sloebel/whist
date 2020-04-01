c0661b783ad825296744963ed936b2f506f81286
Shahar Loebel <shaharl@cb4.com>
2020-04-01 10:37:03 +0300

    migrating some files from js to ts/tsx

--- src/constants/cards.js
+++
@@ -1,7 +0,0 @@
-export const CARDS = {
-  SPADE: 'SPADE',
-  HART: 'HART',
-  DIAMOND: 'DIAMOND',
-  CLUB: 'CLUB',
-  NT: 'NT'
-};
---
+++ src/constants/cards.ts
@@ -0,0 +1,7 @@
+export enum CARDS {
+  SPADE = 'SPADE',
+  HART = 'HART',
+  DIAMOND = 'DIAMOND',
+  CLUB = 'CLUB',
+  NT = 'NT',
+}
