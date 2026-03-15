ff19efd7b3ca153ee719092f5642c0e71e02cfb6
Shahar Loebel <shahar.lebel@gmail.com>
2026-03-15 22:06:56 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whist/precache-manifest.5907c3101ce7741d377110fdbd658cf5.js"
+  "/whist/precache-manifest.3b6710e74cf525300c103b15ca696bde.js"
 );
 
 self.addEventListener('message', (event) => {
